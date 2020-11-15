ff2665221b84b021b6379858caea624f3211612a
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-15 00:34:41 -0300

    Update questão1.sql

--- questão1.sql
+++ questão1.sql
@@ -1,1 +1,8 @@
-
+DELIMITER $
+CREATE OR REPLACE FUNCTION qtd_passageiros(INT codigo) RETURNS INT
+BEGIN
+SET @quantidade := (SELECT num_passageiros WHERE 
+voo.codigo = codigo);
+RETURN @quantidade;
+END $
+DELIMITER;
