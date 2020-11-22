2f942c0b997023637cf19baed5f7400a9493b516
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 21:56:10 -0300

    Update questão3.sql

--- questão3.sql
+++ questão3.sql
@@ -1,1 +1,12 @@
-
+CREATE FUNCTION total_voos(cod_cliente INTEGER)
+RETURNS INTEGER
+AS $$
+DECLARE
+total INTEGER;
+BEGIN
+SELECT INTO total COUNT(*)
+FROM cliente_voo cv
+WHERE cv.cliente=cod_cliente;
+RETURN total;
+END;
+$$ LANGUAGE plpgsql;
