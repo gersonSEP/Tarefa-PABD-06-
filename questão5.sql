04d53fb897a7f9152ecc8258081c3d701aaa6284
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 22:01:35 -0300

    Update questão5.sql

--- questão5.sql
+++ questão5.sql
@@ -1,1 +1,12 @@
-
+CREATE FUNCTION cliente_milha_500(cod_cliente INTEGER)
+RETURNS INTEGER
+AS $$
+DECLARE
+total INTEGER;
+BEGIN
+SELECT total COUNT(*)
+FROM milhas mi
+WHERE mi.quantidade>500;
+RETURN total;
+END;
+$$ LANGUAGE plpgsql;
