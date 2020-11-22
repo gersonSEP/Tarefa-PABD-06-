f5f32601fe76718e768d6ffbc299c464ef8c04a6
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 21:08:50 -0300

    Update questão2.sql

--- questão2.sql
+++ questão2.sql
@@ -1,7 +1,12 @@
 DELIMITER $
-CREATE OR REPLACE FUNCTION classe_cliente(INT codigo, INT codigo) 
+CREATE OR REPLACE FUNCTION classe_cliente(INT codigo_voo, INT codigo_cliente) 
 RETURNS VARCHAR(80)
 BEGIN
+SET @classe := (SELECT INTO @classe cl.classe
+FROM cliente_voo cv
+WHERE cv.voo=cod_voo
+AND cod_cliente=cv.cliente;
+RETURN 
 
 END $
 DELIMITER;
