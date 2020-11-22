81482f6398d94aa529607d6c00685839e7f3ad6f
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 21:59:31 -0300

    Update questão4.sql

--- questão4.sql
+++ questão4.sql
@@ -1,1 +1,11 @@
-
+CREATE FUNCTION total_milhas(cod_cliente INTEGER)
+RETURNS INTEGER
+AS $$
+DECLARE
+total INTEGER;
+BEGIN
+SELECT total SUM(mi.quantidade)
+FROM milhas mi;
+RETURN total;
+END;
+$$ LANGUAGE plpgsql;
