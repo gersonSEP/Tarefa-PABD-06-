7e55110f8df75c1aff695b8b1f69afb8c09f6d99
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 22:14:08 -0300

    Update questão8.sql

--- questão8.sql
+++ questão8.sql
@@ -1,1 +1,10 @@
-
+CREATE FUNCTION nome_milhas(cod_cliente INTEGER)
+RETURNS TABLE(nome CHARACTER VARYING, milhas INTEGER)
+AS $$
+BEGIN
+SELECT cl.nome, mi.quantidade
+FROM cliente cl, milhas mi
+WHERE cl.codigo=cod_cliente
+AND mi.cliente=cod_cliente;
+END;
+$$ LANGUAGE sql;
