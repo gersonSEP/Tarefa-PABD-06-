cb7cb234925956430b2363db925e88de2a399bc2
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-21 22:16:17 -0300

    Update questão9.sql

--- questão9.sql
+++ questão9.sql
@@ -1,1 +1,9 @@
-
+CREATE FUNCTION piloto_voos(cod_piloto INTEGER)
+RETURNS table(nome CHARACTER VARYING, milhas INTEGER)
+AS $$
+BEGIN
+SELECT nome, num_voos
+FROM piloto
+WHERE piloto.codigo=cod_piloto;
+END;
+$$ LANGUAGE sql;
